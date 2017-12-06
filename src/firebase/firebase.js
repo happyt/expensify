db019f6391fbbb027314185b54aa72a565068233
Ian Morton <ian.morton@sky.uk>
2017-12-06 14:46:32 +0000

    Add second firebase facility - (same db so far)

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,14 @@
 import * as firebase from 'firebase'
-import config from '../config'
+
+const config = {
+    apiKey : process.env.FIREBASE_API_KEY,
+    authDomain : process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL : process.env.FIREBASE_DATABASE_URL,
+    projectId : process.env.FIREBASE_PROJECT_ID,
+    storgeBucket : process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId : process.env.FIREBASE_SENDER_ID,
+}
+
 
 firebase.initializeApp(config);
 const database = firebase.database()
